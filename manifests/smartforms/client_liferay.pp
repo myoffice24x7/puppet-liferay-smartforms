9d05d995b1857b9075293afcdb0bd4c19cf4a8ca
Alex Zorin <alex@zor.io>
2013-01-12 14:13:26 +1100

    portal-ext.properties templaet for database

--- manifests/smartforms/client_liferay.pp
+++ manifests/smartforms/client_liferay.pp
@@ -11,37 +11,49 @@ class myoffice::smartforms::client_liferay::get {
   }
 
   exec { 'wget-liferay':
-    command => "/usr/bin/wget --output-document=/home/liferay/liferay.zip ${lr_dl}", 
-    creates => '/home/liferay/liferay.zip',
-    group   => 'liferay',
-    user    => 'liferay',
+    command => "/usr/bin/wget --output-document=/home/${web_user}/liferay.zip ${lr_dl}", 
+    creates => "/home/${web_user}/liferay.zip",
+    group   => "${web_user}",
+    user    => "${web_user}",
     require => Package['wget'],
   }
 
   exec { 'unzip-liferay':
-    command => '/usr/bin/unzip -o /home/liferay/liferay.zip',
-    user    => 'liferay',
-    group   => 'liferay',
-    cwd     => '/home/liferay/',
+    command => "/usr/bin/unzip -o /home/${web_user}/liferay.zip",
+    user    => "${web_user}",
+    group   => "${web_user}",
+    cwd     => "/home/${web_user}/",
     creates => "${lr_root}",
     require => Package['unzip'],
   }
 
-  file { '/home/liferay/liferay-portal':
+  file { "/home/${web_user}/liferay-portal":
     ensure => 'link',
     target => "${lr_root}",
-    owner  => 'liferay',
-    group  => 'liferay',
+    owner  => "${web_user}",
+    group  => "${web_user}",
   }
 
-  Exec['unzip-liferay'] -> File['/home/liferay/liferay-portal']
+  Exec['unzip-liferay'] -> File["/home/${web_user}/liferay-portal"]
+}
+
+class myoffice::smartforms::client_liferay::configure {
+  file { "/home/${web_user}/liferay-portal/tomcat-7.0.27/webapps/ROOT/WEB-INF/classes/portal-ext.properties":
+    owner   => "${web_user}",
+    group   => "${web_user}",
+    content => template('myoffice/portal-ext.erb')
+  }
 }
 
 class myoffice::smartforms::client_liferay(
-  $lr_dl   = $myoffice::smartforms::client_params::lr_dl,
-  $lr_root = $myoffice::smartforms::client_params::lr_root,
+  $lr_dl    = $myoffice::smartforms::client_params::lr_dl,
+  $lr_root  = $myoffice::smartforms::client_params::lr_root,
+  $web_user = $myoffice::smartforms::client_params::web_user,
 ) inherits myoffice::smartforms::client_params {
 
   class { 'myoffice::smartforms::client_liferay::get': }
+  class { 'myoffice::smartforms::client_liferay::configure': }
+
+  Class['myoffice::smartforms::client_liferay::get'] -> Class['myoffice::smartforms::client_liferay::configure']
 
 }
